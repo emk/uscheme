fb848cb6f7ce94c94f8dee27641fdc0cbcdb9a63
Eric Kidd <git@randomhacks.net>
2010-12-04 14:09:32 -0500

    Add test cases for several 'let' corner cases

--- MicroScheme/EvalTests.hs
+++ MicroScheme/EvalTests.hs
@@ -21,6 +21,9 @@ evalPlusFloatIntTest = assertEval (FloatValue 3.0) "(+ 1.0 2)"
 evalPlusFloatFloatTest = assertEval (FloatValue 3.0) "(+ 1.0 2.0)"
 
 evalLetTest = assertEval (IntValue 2) "(let [[x 1]] (+ x x))"
+evalLetOrderTest = assertEval (IntValue 2) "(let [[x 1] [x 2]] x)"
+evalNestedLetTest =
+    assertEval (IntValue 1) "(let [[x 1] [y 2]] (let [[x 3] [y x]] y))"
 
 evalTests =
     test [ "evalIntTest" ~: evalIntTest
@@ -31,5 +34,7 @@ evalTests =
          , "evalPlusFloatIntTest" ~: evalPlusFloatIntTest
          , "evalPlusFloatFloatTest" ~: evalPlusFloatFloatTest
          , "evalLetTest" ~: evalLetTest
+         , "evalLetOrderTest" ~: evalLetOrderTest
+         , "evalNestedLetTest" ~: evalNestedLetTest
          ]
 
