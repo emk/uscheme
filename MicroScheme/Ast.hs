af0f2211bf5d5da0b91648aca5e40d4842424c90
Eric Kidd <git@randomhacks.net>
2010-12-04 14:55:10 -0500

    Correctly build Asts from RuntimeValues

--- MicroScheme/Ast.hs
+++ MicroScheme/Ast.hs
@@ -4,11 +4,15 @@ module MicroScheme.Ast (Ast(Literal), buildAst) where
 
 import MicroScheme.Value
 
+-- |An abstract syntax tree, which is built from raw 'Sexp' objects and
+-- consumed by latter stages of interpreter or compilation.
 data Ast = Literal Value
-         | Primitive String Ast Ast
+         | Primitive String [Ast]
          | Let [(String, Ast)] Ast
          | Body [Ast]
   deriving (Eq, Show)
 
+-- |Construct an Ast from an Sexp.
 buildAst :: Sexp -> Ast
-buildAst sexp = Literal (IntValue 1)
+buildAst (RuntimeValue v) = Literal v
+buildAst ast = error ("Unable to build Ast for " ++ show ast)
