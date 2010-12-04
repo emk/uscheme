358ce27d311588804dc5656c2cfc5ec90ceac523
Eric Kidd <git@randomhacks.net>
2010-12-04 17:00:00 -0500

    Remove "L." from LLVM identifiers
    It's going to get annoying to keep typing that.

--- MicroScheme/Jit.hs
+++ MicroScheme/Jit.hs
@@ -2,19 +2,19 @@ module MicroScheme.Jit (jit) where
 
 import Data.Int
 import Data.Word
-import qualified LLVM.Core as L
-import qualified LLVM.ExecutionEngine as L
+import LLVM.Core hiding (Value)
+import LLVM.ExecutionEngine
 
 import MicroScheme.Ast
 import MicroScheme.Value
 
-mReturn1 :: L.CodeGenModule (L.Function (IO Int32))
+mReturn1 :: CodeGenModule (Function (IO Int32))
 mReturn1 =
-    L.createFunction L.ExternalLinkage $ do
-      L.ret (1::Int32)
+    createFunction ExternalLinkage $ do
+      ret (1::Int32)
 
 jit :: Ast -> IO Value
 jit ast = do
-  return1 <- L.simpleFunction mReturn1
+  return1 <- simpleFunction mReturn1
   value <- return1
   return (IntValue (toInteger value))
