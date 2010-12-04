05198ad0f827b9fe0af0536150df7edadbc11dc4
Eric Kidd <git@randomhacks.net>
2010-12-04 08:29:44 -0500

    Store variables in environment, and fake implementation of 'let'

--- MicroScheme/Eval.hs
+++ MicroScheme/Eval.hs
@@ -1,14 +1,22 @@
 module MicroScheme.Eval (eval) where
 
+import qualified Data.Map as M
+
 import MicroScheme.Value
 
-evalBody (expr:[]) = eval expr
-evalBody (expr:exprs) = do
-  eval expr
-  evalBody exprs
+type Environment = M.Map String Value
 
-evalLet bindings body = do
-  evalBody body
+defaultEnv :: Environment
+defaultEnv = M.empty
+
+evalBody env (expr:[]) = evalExpr env expr
+evalBody env (expr:exprs) = do
+  evalExpr env expr
+  evalBody env exprs
+
+evalLet env bindings body = do
+  evalBody newEnv body
+    where newEnv = M.insert "x" (IntValue 1) env
 
 -- Evaluate a binary arithmetic operation, promoting our arguments so that
 -- the types match.
@@ -26,17 +34,24 @@ arithmeticBinOp name intOp floatOp x y = op x y
                     show x ++ " " ++ show y)
 
 -- Evaluate a function call.
-evalCall "+" (x:y:[]) = return (arithmeticBinOp "+" (+) (+) x y)
-evalCall name args =
+primCall "+" (x:y:[]) = return (arithmeticBinOp "+" (+) (+) x y)
+primCall name args =
   error ("Don't know how to call " ++ name ++ " with " ++ show args)
+
+-- Evaluate a Scheme expression in 'env'.
+evalExpr :: Environment -> Ast -> IO Value
+evalExpr env (Literal value) = return value
+evalExpr env (List ((Symbol "let"):bindings:body)) = do
+  evalLet env bindings body
+evalExpr env (List ((Symbol name):args)) = do
+  args' <- mapM (evalExpr env) args
+  primCall name args'
+evalExpr env (Symbol name) =
+    case M.lookup name env of
+      Just val -> return val
+      Nothing  -> error ("Unbound variable: " ++ name)
+evalExpr env ast = error ("Don't know how to eval " ++ show ast)
 
 -- |Evaluate a Scheme expression.
 eval :: Ast -> IO Value
-eval (Literal value) = return value
-eval (List ((Symbol "let"):bindings:body)) = do
-  evalLet bindings body
-eval (List ((Symbol name):args)) = do
-  args' <- mapM eval args
-  evalCall name args'
-eval (Symbol name) = return (IntValue 1)
-eval ast = error ("Don't know how to eval " ++ show ast)
+eval ast = evalExpr defaultEnv ast
