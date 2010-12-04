776640a700658b5cc66df0da8005db16b83429a4
Eric Kidd <git@randomhacks.net>
2010-12-04 16:57:32 -0500

    Use LLVM to JIT a function which always returns 1

--- MicroScheme/Jit.hs
+++ MicroScheme/Jit.hs
@@ -1,10 +1,20 @@
 module MicroScheme.Jit (jit) where
 
+import Data.Int
+import Data.Word
 import qualified LLVM.Core as L
 import qualified LLVM.ExecutionEngine as L
 
 import MicroScheme.Ast
 import MicroScheme.Value
 
+mReturn1 :: L.CodeGenModule (L.Function (IO Int32))
+mReturn1 =
+    L.createFunction L.ExternalLinkage $ do
+      L.ret (1::Int32)
+
 jit :: Ast -> IO Value
-jit ast = return (IntValue 1)
+jit ast = do
+  return1 <- L.simpleFunction mReturn1
+  value <- return1
+  return (IntValue (toInteger value))
